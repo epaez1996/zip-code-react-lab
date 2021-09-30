9103df50e6c43b98d1d3d5acd646ce583bc6456b
Edward Paez <Paez.edward5@gmail.com>
2021-09-30 18:55:18 -0400

    Finished zipcode lab part1

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,89 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-
-function City(props) {
-  return (<div>This is the City component</div>);
+function ZipSearchField({ zipCode, handleZipCodeChange }) {
+  return (
+    <div
+      style={{
+        color: "#222",
+        fontWeight: "bold",
+        textAlign: "center",
+        margin: "1rem",
+      }}
+    >
+      Zip Code:{" "}
+      <input
+        type="text"
+        placeholder="Try 10016"
+        value={zipCode}
+        onChange={handleZipCodeChange}
+      />
+    </div>
+  );
 }
 
-function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
-}
-
+const City = ({
+  state,
+  location,
+  estimatedPopulation,
+  totalWages,
+  locationText,
+}) => {
+  return (
+    <div className="city-container">
+      <div className="header-container">
+        <h4>{locationText}</h4>
+      </div>
+      <div>
+        <ul className="info-container">
+          <li>State: {state}</li>
+          <li>Location: {location}</li>
+          <li>Population (estimated): {estimatedPopulation}</li>
+          <li>Total Wages: {totalWages}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { zipCode: "", zipData: [] };
+    this.handleZipCodeChange = this.handleZipCodeChange.bind(this);
+  }
+
+  handleZipCodeChange(e) {
+    if (e.target.value.length === 5) {
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${e.target.value}`)
+        .then((response) => response.json())
+        .then((data) => this.setState({ ...this.state, zipData: data }))
+        .catch((err) => console.log(err));
+    }
+    this.setState({ zipCode: e.target.value });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField
+          zipCode={this.state.zipCode}
+          handleZipCodeChange={this.handleZipCodeChange}
+        />
         <div>
-          <City />
-          <City />
+          {this.state.zipData.map((data, i) => (
+            <City
+              key={i}
+              location={data.Location}
+              state={data.State}
+              estimatedPopulation={data.EstimatedPopulation}
+              totalWages={data.TotalWages}
+              locationText={data.LocationText}
+            />
+          ))}
         </div>
       </div>
     );
